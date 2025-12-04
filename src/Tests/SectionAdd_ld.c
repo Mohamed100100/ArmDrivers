e16fb81f19fd1632f97fc104f8279b2140f20bef
Mohamed100100 <muhammad.ahmed302@eng-st.cu.edu.eg>
2025-12-04 16:44:44 +0200

    this commit has my custom linker script and test on it to add section in RAM and ROM

---
+++ src/Tests/SectionAdd_ld.c
@@ -0,0 +1,20 @@
+
+
+
+#include "LIB/stdtypes.h"
+#include "linkerScriptTest/SectionAdd_ld.h"
+
+// Non volatile so it will be 10 
+volatile uint32_t __attribute__((section(".led_cfg_flash_section")))  LED_CFG_Flash =10;
+// volatile so it will be garbage
+uint32_t __attribute__((section(".led_cfg_ram_section")))  LED_CFG_Ram = 10 ;
+
+
+void testLinkerScript(){
+    printf("%x",&LED_CFG_Flash);
+    printf("%d",LED_CFG_Ram);
+    // ignore editing
+    LED_CFG_Flash = 30;
+    // editing successfully
+    LED_CFG_Ram   = 30;
+}
